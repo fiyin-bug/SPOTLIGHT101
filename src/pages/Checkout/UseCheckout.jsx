f20c32441d3d3318126e1e3afa252b1800364979
david <jinadudavid118gmail.com>
2025-03-06 20:54:09 +0100

    USECHECKOUT

--- src/pages/Checkout/UseCheckout.jsx
+++ src/pages/Checkout/UseCheckout.jsx
@@ -1,6 +1,6 @@
-// "use client"
 
-import { useState } from "react"
+
+import { useState } from "react";
 
 function useCheckout() {
   // Initialize state with default values, not from localStorage
