f0c03fba3bebe81e8de1495a6a4a0b27eb05b62d
Jinadu David Mofiyinfoluwa <109163420+fiyin-bug@users.noreply.github.com>
2025-03-25 00:17:29 +0100

    EDITS

--- src/pages/Checkout/DiscountHandler.jsx
+++ src/pages/Checkout/DiscountHandler.jsx
@@ -17,7 +17,7 @@ function DiscountHandler({ referralCode, originalTotal, onDiscountApplied }) {
     "MONNIE",
     "GUS",
     "HXC",
-    "FINNIE"
+    "FINNIE",
     "TUZO1960",
     "JIGGY1536",
     "VASTIFE",
