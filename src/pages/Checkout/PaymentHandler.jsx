00f5a112039ac9d92a4c05eb747576a6224d6191
Jinadu David Mofiyinfoluwa <109163420+fiyin-bug@users.noreply.github.com>
2025-04-17 15:15:16 +0000

    Update PaymentHandler.jsx

--- src/pages/Checkout/PaymentHandler.jsx
+++ src/pages/Checkout/PaymentHandler.jsx
@@ -1,4 +1,4 @@
-​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​import { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PaystackPop from "@paystack/inline-js";
 import * as Yup from "yup";
